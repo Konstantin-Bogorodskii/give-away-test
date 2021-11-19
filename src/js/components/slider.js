952d4b4324c024f4fc9b88fc20bb6b6bc3e678d0
kbogorodsky <konstantin.bogorodsky@gmail.com>
2021-11-19 22:27:58 +0300

    added swiper for project

---
+++ src/js/components/slider.js
@@ -0,0 +1,10 @@
+const prodSwiper = new Swiper(".slider__container", {
+  slidesPerView: 1,
+  spaceBetween: 20,
+
+  // Navigation arrows
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
