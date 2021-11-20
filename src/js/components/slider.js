7440abeb3934df56cb5dfc23e8384d92a1f48098
kbogorodsky <konstantin.bogorodsky@gmail.com>
2021-11-20 21:40:23 +0300

    created slider added styles and media queries

--- src/js/components/slider.js
+++ src/js/components/slider.js
@@ -4,7 +4,15 @@ const prodSwiper = new Swiper(".slider__container", {
 
   // Navigation arrows
   navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
+    nextEl: ".slider-preview__next",
+    prevEl: ".slider-preview__prev",
   },
+});
+
+const prodImages = document.querySelectorAll(".slider-preview__item");
+prodImages.forEach((item) => {
+  item.addEventListener("click", (e) => {
+    const idx = +e.currentTarget.dataset.index;
+    prodSwiper.slideTo(idx);
+  });
 });
